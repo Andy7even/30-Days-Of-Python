1c8d71aed9f799284e4733d7b888cdd832e86375
Andy7even <andysarpong30@gmail.com>
2026-03-23 01:38:07 +0000

    Update variables.py

--- images/day__2/variables.py
+++ images/day__2/variables.py
@@ -1,1 +1,10 @@
-
+first_name = 'Alberta'
+last_name = 'Osei'
+full_name = 'Alberta Osei'
+country = 'Canada'
+city = 'Sudbury'
+age = '19'
+year = '2009'
+is_miner = 'true'
+is_true = 'miner'
+is_light_on = 'false'
